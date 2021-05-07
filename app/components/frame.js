07d837b7705a2504d8b858551b71d2e227285300
Frank Mejzlik <frankmejzlik@gmail.com>
2021-05-07 12:20:57 +0200

    Redirect media requests to data server. Reformat.

--- app/components/frame.js
+++ app/components/frame.js
@@ -4,6 +4,7 @@ import { action } from "@ember/object";
 
 import { inject as service } from "@ember/service";
 
+import ENV from "somhunter-ui/config/environment";
 import { EVENTS, ELEM_IDS } from "../constants";
 import LOG from "../logger";
 import utils from "../utils";
@@ -67,4 +68,6 @@ export default class FrameComponent extends Component {
 
     /* Member variables */
     @service actionManager;
+
+    thumbsUrlPrefix = ENV.dataServerUrl + "/thumbs/";
 }
