1bd4cbed59fd77a3ca53ea05da068fed83e400fe
Frank Mejzlik <frankmejzlik@gmail.com>
2021-05-10 10:05:42 +0200

    Update cd to script's dir.

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+ABSOLUTE_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 printf "\tInstalling 'somhunter-ui'...\n"
 
 
--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ABSOLUTE_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/"
+ABSOLUTE_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 cd $ABSOLUTE_PATH # CD to script dir
 
 echo "Running 'somhunter-ui'..."
