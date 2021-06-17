a202a945cfbaa074a4114f4b489076697260ab74
Frank Mejzlik <frankmejzlik@gmail.com>
2021-06-17 22:24:59 +0200

    temporary VBS edits

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-ABSOLUTE_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-cd $ABSOLUTE_PATH # CD to script dir
-
-echo "Running 'somhunter-ui'..."
-#npx ember serve
-#serve -p 9000 ./dist
-http-server-spa-with-auth ./dist index.html 9000 x som hunter
