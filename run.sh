161e5b75251253fc88cf5b380b1913a20939f531
Frank Mejzlik <frankmejzlik@gmail.com>
2021-06-17 21:59:55 +0200

    Change UI serving utility.

--- run.sh
+++ run.sh
@@ -4,4 +4,6 @@ ABSOLUTE_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 cd $ABSOLUTE_PATH # CD to script dir
 
 echo "Running 'somhunter-ui'..."
-npx ember serve
+#npx ember serve
+#serve -p 9000 ./dist
+http-server-spa-with-auth ./dist index.html 9000 x som hunter
