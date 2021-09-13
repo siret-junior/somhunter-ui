b591775e61f3d48e06b6d229eebaa47fd4c9daac
Frank Mejzlik <frankmejzlik@gmail.com>
2021-09-13 17:27:49 +0200

    Update npm modules.

--- app/initializers/main.js
+++ app/initializers/main.js
@@ -3,11 +3,12 @@ import { registerDeprecationHandler } from "@ember/debug";
 
 export function initialize() {
     registerDeprecationHandler((message, options, next) => {
-        if (options && options.until && options.until !== "2.0.0") {
-            return;
-        } else {
-            next(message, options);
-        }
+        return;
+        // if (options && options.until && options.until !== "2.0.0") {
+        //     return;
+        // } else {
+        //     next(message, options);
+        // }
     });
 }
 
