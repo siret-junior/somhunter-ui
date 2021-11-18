f04a25d7f4199093608b2fa7a55045e162c9e50b
Frank Mejzlik <frankmejzlik@gmail.com>
2021-11-18 08:24:27 +0100

    Adjust for LSC.

--- run.sh
+++ run.sh
@@ -4,9 +4,9 @@ ABSOLUTE_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 cd $ABSOLUTE_PATH # CD to script dir
 
 echo "Running 'somhunter-ui'..."
-npx ember serve       #<< DEV
+#npx ember serve       #<< DEV
 
 #npx ember build      #<< PRODUCTION
 #serve -p 9000 ./dist #<< PRODUCTION
 
-#http-server-spa-with-auth ./dist index.html 8080 x som hunter
+http-server-spa-with-auth ./dist index.html 8080 x som hunter
