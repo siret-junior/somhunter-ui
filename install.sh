388676b98163dc9d6a5dc6597d0a19d66ff11b84
Frank Mejzlik <frankmejzlik@gmail.com>
2021-12-04 20:48:30 +0100

    Edit install script.

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ ABSOLUTE_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 cd ${ABSOLUTE_PATH}
 printf "\tInstalling 'somhunter-ui'...\n"
 
+npx browserslist@latest --update-db > /dev/null
 npm install > /dev/null
 npx ember build > /dev/null
 
