d5243ca2c7e714dcf807b336a441c673fd33caf9
Frank Mejzlik <frankmejzlik@gmail.com>
2021-12-04 20:12:28 +0100

    Fix docker build.

--- install.sh
+++ install.sh
@@ -3,8 +3,7 @@ ABSOLUTE_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 cd ${ABSOLUTE_PATH}
 printf "\tInstalling 'somhunter-ui'...\n"
 
-npm install
-#npm install -g ember-cli
-npx ember build
+npm install > /dev/null
+npx ember build > /dev/null
 
 printf "\tDone installing 'somhunter-ui'...\n"
